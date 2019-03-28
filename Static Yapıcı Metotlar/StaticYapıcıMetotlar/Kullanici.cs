31f023ff0f1ae9d7ce4f14fa4f6b984df689c4e5
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-03-28 23:23:04 +0300

    Add files via upload

---
+++ Static Yapıcı Metotlar/StaticYapıcıMetotlar/Kullanici.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticYapıcıMetotlar
+{
+    class Kullanici
+    {
+        /*
+         * Kullanıcı sınıfının 2 adet yapıcı metotdu bulunmaktadır. Bunlardan static olanı
+         * "kullaniciSayisi" adlı static değişkenin başlangoç değerlerini atarken, static olmayanı
+         * "nick" ve "eMail" değişkenlerinin atama işlemlerini gerçekleştirmektedir.
+         * 
+         * Kullanıcı sınıfından ilk nesne oluşturulduğunda hem static yapıcı metot hemde static
+         * olmayan yapıcı metot çalışacaktır (ilk static yapıcı metot çalışır). İlk nesneden sonra
+         * oluşturulacak diğer tüm nesnelerde sadece static olmayan yapıcı metot çağıralacaktır.
+         * Yani static yapıcı metot sadece ilk nesne oluşturulduğunda çalışır.
+         */
+        static int kullaniciSayisi;
+        public static int KullaniciSayisi
+        {
+            get
+            {
+                return kullaniciSayisi;
+            }
+        }
+
+        string nick;
+        string eMail;
+      
+        static Kullanici()
+        {
+            kullaniciSayisi = 0;
+        }
+
+        public Kullanici(string nick, string eMail)
+        {
+            this.nick = nick;
+            this.eMail = eMail;
+            kullaniciSayisi++;
+        }
+    }
+}
---
+++ Static Yapıcı Metotlar/StaticYapıcıMetotlar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticYapıcıMetotlar
+{
+    class Program
+    {
+        /*
+         * Static Yapıcı Metotlar
+         * 
+         * Yapıcı metotlar da normal metotlar gibi static olarak tanımlanabilirler. Sınıfın static
+         * değişkenleri ile ilgili işlemler static yapıcı metodun içerisinde yapılabilir. Static
+         * yapıcı metotlar parametre almazlar ve erişim belirleyicileri yoktur.
+         */
+        static void Main(string[] args)
+        {
+            //Online Kullanıcı Sayısı: 0
+            Console.WriteLine("Online Kullanıcı Sayısı: {0}",Kullanici.KullaniciSayisi);
+
+            Kullanici k1 = new Kullanici("İlkcan Doğan", "deneme@mail.com");
+            Kullanici k2 = new Kullanici("Cafer Gün","deneme2@gmail.com");
+
+            //Online kullanıcı Sayısı: 2
+            Console.WriteLine("Online Kullanıcı Sayısı: {0}", Kullanici.KullaniciSayisi);
+
+            Console.ReadLine();
+
+        }
+    }
+}
